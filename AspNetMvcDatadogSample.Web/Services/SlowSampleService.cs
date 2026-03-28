c4664a3829e05e41dddc89d705d24dc490a5103e
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2026-03-28 22:20:51 +0900

    update 更新。

--- AspNetMvcDatadogSample.Web/Services/SlowSampleService.cs
+++ AspNetMvcDatadogSample.Web/Services/SlowSampleService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Threading;
 using Datadog.Trace;
 
@@ -6,6 +7,8 @@ namespace AspNetMvcDatadogSample.Web.Services
 {
     public class SlowSampleService
     {
+        private static readonly object SharedLock = new object();
+
         public string Execute(int delayMs)
         {
             using (var scope = Tracer.Instance.StartActive("sample.slow-service"))
@@ -14,11 +17,12 @@ namespace AspNetMvcDatadogSample.Web.Services
                 scope.Span.SetTag("sample.delay_ms", delayMs.ToString());
                 scope.Span.SetTag("sample.simulated", "true");
 
-                Thread.Sleep(delayMs);
+                var result = SimulateLogicalDelay(delayMs);
 
                 return string.Format(
-                    "SlowSampleService completed. delayMs={0}, utc={1:O}",
+                    "SlowSampleService completed. delayMs={0}, result={1}, utc={2:O}",
                     delayMs,
+                    result,
                     DateTime.UtcNow);
             }
         }
@@ -34,7 +38,7 @@ namespace AspNetMvcDatadogSample.Web.Services
 
                 try
                 {
-                    Thread.Sleep(delayMs);
+                    SimulateLogicalDelay(delayMs);
 
                     throw new InvalidOperationException(
                         string.Format("Datadog exception test. delayMs={0}", delayMs));
@@ -47,6 +51,92 @@ namespace AspNetMvcDatadogSample.Web.Services
 
                     throw;
                 }
+            }
+        }
+
+        private static string SimulateLogicalDelay(int delayMs)
+        {
+            var contentionMs = Math.Max(50, delayMs / 5);
+            var cpuIterations = Math.Max(10000, delayMs * 400);
+            var allocationLoops = Math.Max(5, delayMs / 100);
+
+            SimulateLockContention(contentionMs);
+            var cpuScore = SimulateCpuWork(cpuIterations);
+            var textLength = SimulateAllocationWork(allocationLoops);
+
+            return string.Format(
+                "contentionMs={0}, cpuIterations={1}, textLength={2}, cpuScore={3:F2}",
+                contentionMs,
+                cpuIterations,
+                textLength,
+                cpuScore);
+        }
+
+        private static void SimulateLockContention(int holdMs)
+        {
+            using (Tracer.Instance.StartActive("sample.lock-contention"))
+            using (var ready = new ManualResetEventSlim(false))
+            {
+                var worker = new Thread(() =>
+                {
+                    lock (SharedLock)
+                    {
+                        ready.Set();
+                        Thread.Sleep(holdMs);
+                    }
+                });
+
+                worker.IsBackground = true;
+                worker.Start();
+
+                ready.Wait();
+
+                lock (SharedLock)
+                {
+                }
+
+                worker.Join();
+            }
+        }
+
+        private static double SimulateCpuWork(int iterations)
+        {
+            using (Tracer.Instance.StartActive("sample.cpu-work"))
+            {
+                double total = 0;
+
+                for (var i = 1; i <= iterations; i++)
+                {
+                    total += Math.Sqrt(i) * Math.Sin(i * 0.01d);
+                }
+
+                return total;
+            }
+        }
+
+        private static int SimulateAllocationWork(int loops)
+        {
+            using (Tracer.Instance.StartActive("sample.allocation-work"))
+            {
+                var builder = new StringBuilder();
+
+                for (var i = 0; i < loops; i++)
+                {
+                    builder.Append(i);
+                    builder.Append(':');
+
+                    for (var j = 0; j < 12; j++)
+                    {
+                        builder.Append(Guid.NewGuid().ToString("N"));
+                    }
+
+                    builder.AppendLine();
+                }
+
+                var text = builder.ToString();
+                var normalized = text.ToUpperInvariant().Replace("A", "a");
+
+                return normalized.Length;
             }
         }
     }
