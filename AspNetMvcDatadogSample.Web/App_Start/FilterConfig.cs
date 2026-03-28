b0c8dcefa093fc0edbb2f83eafdbad2955266b7f
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2026-03-28 11:26:09 +0900

    add 新規追加。

---
+++ AspNetMvcDatadogSample.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcDatadogSample.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNetMvcDatadogSample.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvcDatadogSample.Web.MvcApplication" Language="C#" %>
